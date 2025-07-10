440b1c2ef0438788e008d11e58eb158e25663e78
Claire Samson <samsonclaire061@gmail.com>
2025-07-11 00:41:25 +0800

    feat: remove code

--- include/certificate.php
+++ include/certificate.php
@@ -165,7 +165,6 @@ Feel free to explore the proof of my dedication below. Every badge, every lesson
     <img src="/img/certificates/18.jpg" alt="Certificate 18" onclick="openFullscreen(this.src)" />
     <img src="/img/certificates/19.png" alt="Certificate 19" onclick="openFullscreen(this.src)" />
     <img src="/img/certificates/20.PNG" alt="Certificate 20" onclick="openFullscreen(this.src)" />
-    <img src="/img/certificates/23.jpg" alt="Certificate 23" onclick="openFullscreen(this.src)" />
     <img src="/img/certificates/27.jpg" alt="Certificate 27" onclick="openFullscreen(this.src)" />
     <img src="/img/certificates/28.jpg" alt="Certificate 28" onclick="openFullscreen(this.src)" />
   </div>
